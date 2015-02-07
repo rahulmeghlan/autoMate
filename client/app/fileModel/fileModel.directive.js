35a207c7a8cd5c7835fcdbf229b339fca440bfae
Rahul Meghlan <rahulmeghlan@gmail.com>
2015-02-07 23:17:43 +0530

    fileModel directive missed in the earlier commit

---
+++ client/app/fileModel/fileModel.directive.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('angularFullstackApp')
+    .directive('fileModel', ['$parse', function ($parse) {
+        return {
+            template: '<div></div>',
+            restrict: 'EA',
+            link: function (scope, element, attrs) {
+                var model = $parse(attrs.fileModel);
+                var modelSetter = model.assign;
+                element.bind("change", function () {
+                    scope.$apply(function () {
+                        modelSetter(scope, element[0].files[0]);
+                    });
+                });
+                element.text('this is the fileModel directive');
+            }
+        };
+    }]);
