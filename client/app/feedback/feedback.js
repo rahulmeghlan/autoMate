c4ec35ca4bd3ed36651f358185f8923ea0047c94
Rahul Meghlan <rahulmeghlan@gmail.com>
2015-02-09 15:33:09 +0530

    AM-1, unable to select an auto-number : FIXED

--- client/app/feedback/feedback.js
+++ client/app/feedback/feedback.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('angularFullstackApp')
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/feedback', {
-        templateUrl: 'app/feedback/feedback.html',
-        controller: 'FeedbackCtrl'
-      });
-  });
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/feedback', {
+                templateUrl: 'app/feedback/feedback.html',
+                controller: 'FeedbackCtrl'
+            })
+            .when('/feedback/:id', {
+                templateUrl: 'app/feedback/feedback_id.html',
+                controller: 'FeedbackCtrl'
+            });
+    });
